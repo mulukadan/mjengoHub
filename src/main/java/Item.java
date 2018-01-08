1cce17f06e8ce358d619c796f6f0a20643b17eb8
mulukadan <kadan.md@gmail.com>
2018-01-08 16:31:24 +0300

    first commit

---
+++ src/main/java/Item.java
@@ -0,0 +1,93 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Item{
+  private int id;
+  private String name;
+  private String location;
+  private String cost;
+  private String img;
+  private int sellerid;
+
+  public Item(String name, String location, String cost, String img, int sellerid){
+    this.name = name.toUpperCase();
+    this.location = location;
+    this.cost = cost;
+    this.img = img;
+    this.sellerid = sellerid;
+
+  }
+  public int getId(){
+    return id;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getLocation(){
+    return location;
+  }
+  public String getCost(){
+    return cost;
+  }
+
+  public Stylist getSeller() {
+    int id = sellerid;
+    Seller seller;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sellers where id=:id";
+       seller = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Seller.class);
+       }
+    return seller;
+  }
+
+  public static List<Item> all() {
+      String sql = "SELECT id, name, location, cost, img, sellerid FROM items ORDER BY name";
+      try(Connection con = DB.sql2o.open()) {
+        return con.createQuery(sql).executeAndFetch(Item.class);
+      }
+  }
+  public void save() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "INSERT INTO items(name, location, cost, img, sellerid) VALUES (:name, :location, :cost, :img, :sellerid)";
+     this.id = (int) con.createQuery(sql, true)
+       .addParameter("name", this.name)
+       .addParameter("location", this.location)
+       .addParameter("cost", this.cost)
+       .addParameter("img", this.img)
+       .addParameter("sellerid", this.sellerid)
+       .executeUpdate()
+       .getKey();
+    }
+  }
+  public static Item find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM items where id=:id";
+      Item item = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Item.class);
+      return item;
+    }
+  }
+  public void update(String name, String gender, String contact, int stylist_id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE items SET name = :name, location = :location, cost = :cost, img = :img, sellerid = :sellerid WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("name", this.name)
+        .addParameter("location", this.location)
+        .addParameter("cost", this.cost)
+        .addParameter("img", this.img)
+        .addParameter("sellerid", this.sellerid)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM items WHERE id = :id;";
+    con.createQuery(sql).addParameter("id", id).executeUpdate();
+    }
+  }
+
+}
---
+++ src/main/java/Seller.java
@@ -0,0 +1,93 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Seller{
+  private int id;
+  private String name;
+  private String password;
+  private String email;
+  private String phone;
+
+
+  public Seller(String name, String password, String email, String contact){
+    this.name = name.toUpperCase();
+    this.password = password;
+    this.email = email;
+    this.phone = phone;
+
+  }
+  public int getId(){
+    return id;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getPassword(){
+    return password;
+  }
+  public String getEmail(){
+    return email;
+  }
+  public String getPhone(){
+    return phone;
+  }
+  public static List<Seller> all() {
+      String sql = "SELECT id, name, password, email, phone FROM sellers ORDER BY name";
+      try(Connection con = DB.sql2o.open()) {
+        return con.createQuery(sql).executeAndFetch(Seller.class);
+      }
+  }
+  public void save() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "INSERT INTO sellers(name, password, email, phone) VALUES (:name, :password, :email, :phone)";
+     this.id = (int) con.createQuery(sql, true)
+       .addParameter("name", this.name)
+       .addParameter("password", this.password)
+       .addParameter("email", this.email)
+       .addParameter("phone", this.phone)
+       .executeUpdate()
+       .getKey();
+    }
+  }
+  public static Seller find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sellers where id=:id";
+      Stylist stylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Seller.class);
+      return Seller;
+    }
+  }
+  public void update(String name, String gender, String contact) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE sellers SET name = :name, password = :password, email = :email , phone = :phone WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("password", password)
+        .addParameter("email", email)
+        .addParameter("phone", phone)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM sellers WHERE id = :id;";
+    con.createQuery(sql).addParameter("id", id).executeUpdate();
+    }
+    //Assigning client_id to 0 for Clients allocated to the deleted sTYLIST
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM items WHERE sellerid = :id;";
+      con.createQuery(sql).addParameter("id", id).executeUpdate();
+    }
+  }
+  public List<Items> getItems() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "SELECT * FROM items where sellerid=:id ORDER BY name";
+        return con.createQuery(sql)
+          .addParameter("id", this.id)
+          .executeAndFetch(Item.class);
+      }
+    }
+
+}
